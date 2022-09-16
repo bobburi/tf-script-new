f162e8e7a97b2b259b5f1bb82b1313eec1c318db
rajesh bobburi <bobburirajesh006@gmail.com>
2022-09-16 18:24:15 +0530

    addded

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "google" {
+ project = "prod-bayer-360512"
+ credentials = "var.sa-account"
+
+
+}
+
+resource "google_compute_network" "vpc_network" {
+  project                 = "my-project-name"
+  name                    = "vpc-network"
+  auto_create_subnetworks = true
+  mtu                     = 1460
+}
